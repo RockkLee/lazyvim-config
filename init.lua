b95b6e6f92a1d91c21ce34eb718ad19e47fccd01
SamLee <sam.lee@mobiigenius.com>
2024-11-18 14:06:30 +0800

    Add config of Telescope

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("telescope").setup({
+  defaults = {
+    layout_config = {
+      width = 0.99, -- Adjust the width (0.75 = 75% of the window)
+      height = 0.99, -- Adjust the height (0.8 = 80% of the window)
+    },
+  },
+})
