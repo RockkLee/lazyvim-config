10e547e737dd034cb0030e66fa01c72effb04aa1
hiddenlotus <sam880103@gmail.com>
2024-11-28 01:01:44 +0800

    Add the config of the debugger and GO LSP

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.go" }, -- golang extension
+    { import = "lazyvim.plugins.extras.dap.core" }, -- debugger extension
     -- import/override with your plugins
     { import = "plugins" },
   },
