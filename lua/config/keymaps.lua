f7c8204e4d984dc896c897c2df4fa93f1d917350
hiddenlotus <sam880103@gmail.com>
2024-12-21 17:59:28 +0800

    Modify the configuration of hotkeys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
 local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
 
 -- Set Ctrl + w to close the current buffer
-map("n", "<C-w>", ":bd<CR>", opts)
+map("n", "W", ":bd<CR>", { noremap = true, silent = true })
